fdcffbd87304ddc239fb61c824a18b07600e0df3
Matt Chun-Lum <matt@questionable.engineering>
2025-07-26 10:52:13 -0700

    chore: precise dbg

--- reactive_stores_macro/src/lib.rs
+++ reactive_stores_macro/src/lib.rs
@@ -278,20 +278,24 @@ fn field_to_tokens(
         quote! { #ident }
     };
 
-    dbg!(name);
-    dbg!(generics);
-    dbg!(any_store_field);
-    dbg!(ty);
-
     if let Some(modes) = modes {
         if modes.len() == 1 {
             let mode = &modes[0];
             match mode {
                 SubfieldMode::Keyed(keyed_by, key_ty) => {
+                    dbg!(keyed_by);
+                    dbg!(key_ty);
+                    dbg!(name);
+                    dbg!(generics);
+                    dbg!(any_store_field);
+                    dbg!(ty);
+
                     let signature = quote! {
                         #[track_caller]
                         fn #ident(self) ->  #library_path::KeyedSubfield<#any_store_field, #name #generics, #key_ty, #ty>
                     };
+
+                    dbg!(&signature);
                     return if include_body {
                         quote! {
                             #signature {
